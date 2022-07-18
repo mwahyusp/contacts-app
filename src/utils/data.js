6d68a6d8808a76b145937df23b8c737849d6c782
mwahyusp <mwahyu73@gmail.com>
2022-07-18 10:14:27 +0700

    change data
    change data

--- src/utils/data.js
+++ src/utils/data.js
@@ -2,21 +2,21 @@ const getData = () => {
   return [
     {
       id: 1,
-      name: "Dimas Saputra",
-      tag: "dimasmds",
-      imageUrl: "/images/dimasmds.jpeg",
+      name: "Danang Eka Alfianto",
+      tag: "danangekal",
+      imageUrl: "/images/danangekal.jpeg",
     },
     {
       id: 2,
-      name: "Arif Faizin",
-      tag: "arifaizin",
-      imageUrl: "/images/arifaizin.jpeg",
+      name: "Sigit Setiaji",
+      tag: "sigitsetiaji",
+      imageUrl: "/images/sigitsetiaji.jpeg",
     },
     {
       id: 3,
-      name: "Rahmat Fajri",
-      tag: "rfajri27",
-      imageUrl: "/images/rfajri27.jpeg",
+      name: "Falgi Faisal",
+      tag: "falgifaisal",
+      imageUrl: "/images/falgifaisal.jpeg",
     },
   ];
 };
